2e5ba59ba4eee0af9f82a1714b506aeb6f113034
Bocheng <779658284@qq.com>
2022-06-26 14:44:01 +0100

    add the ranking chart

--- frontEnd/VisualCOVID/src/components/charts/world_map.vue
+++ frontEnd/VisualCOVID/src/components/charts/world_map.vue
@@ -29,6 +29,11 @@
         <el-option label="Total Death" value="total_2d_death"></el-option>
         <el-option label="Total Confirm" value="total_2d"></el-option>
       </el-select>
+      <span style="color:black;font-weight:bolder;padding-left: 10px"> Current Graphics Type:</span>
+      <el-select v-model="optionType" placeholder="data type" @change="switchGraphics">
+        <el-option label="World Map" value="map"></el-option>
+        <el-option label="Bar Graph" value="bar"></el-option>
+      </el-select>
     </div>
   </el-card>
 </template>
@@ -49,13 +54,19 @@ export default {
         NewConfirmed:[],
         NewDeaths:[]
       },
+      data:[],
       toolBarText :"Total Confirmed: ",
       toolBarText2:"Total Deaths: ",
-      value: 'total_2d'
+      value: 'total_2d',
+      optionType:'map'
     };
   },
   methods: {
     onSubmit()
+    {
+      this.runWorldMap()
+    },
+    switchGraphics()
     {
       this.runWorldMap()
     },
@@ -70,18 +81,22 @@ export default {
         {
           let data = processPostmanAPIEarthData(res,that.value)
           that.summaryData = res.Global
+          that.data = data
           that.DateCurr = that.summaryData.Date.split('T')[0]
-          that.drawChart(that,data)
+          that.drawChart(that,data,that.optionType)
         }
       })
     },
-    drawChart (context,data) {
+    drawChart (context,data,optionType) {
       let chart = echarts.init(document.getElementById('chart'),'vintage',
         {locale:'EN'})
       window.addEventListener('resize', function () {
         chart.resize()
       })
       chart.hideLoading();
+      data.sort(function (a, b) {
+        return a.value[2] - b.value[2];
+      });
       // draw the chart
       let option = {
         // main title
@@ -104,7 +119,6 @@ export default {
           containLabel: true
         },
         tooltip: {
-
           trigger: 'item', // Trigger type, data item graph trigger,
           // mainly used in scatter charts, pie charts and other charts without category axes
           formatter: function (val) {
@@ -157,10 +171,82 @@ export default {
           }
         ]
       }
+      const barOption = {
+        title: {
+          text: 'The latest ranking chart of coronavirus (COVID-19) cases by country',
+          top: 10,
+          left: 'center',
+          textStyle: {
+            fontSize: 24,
+            fontWeight: 600,
+            color: '#000'
+          }
+        },
+        xAxis: {
+          type: 'value',
+          name: 'Cases',
+        },
+        grid: {
+          top: '12%',
+          left: '1%',
+          right: '10%',
+          containLabel: true
+        },
+        calculable: true,
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+            label: {
+              show: true
+            }
+          }
+        },
+        dataZoom: [
+          {
+            show: true,
+            start: 0,
+            end: 100
+          },
+          {
+            show: true,
+            start: 94,
+            end: 100,
+            yAxisIndex: 0,
+            filterMode: 'empty',
+            width: 30,
+            height: '80%',
+            showDataShadow: false,
+            left: '93%'
+          }
+        ],
+        yAxis: {
+          type: 'category',
+          axisLabel: {
+            rotate: 30
+          },
+          data: data.map(function (item) {
+            return item.name;
+          })
+        },
+        animationDurationUpdate: 1000,
+        series: {
+          type: 'bar',
+          id: 'People Number',
+          data: data.map(function (item) {
+            return item.value[2];
+          }),
+          universalTransition: true,
+          itemStyle: {
+            color: '#1d86ee'
+          }
+        }
+      };
       if(context.value==="total_2d_death")
       {
         option.visualMap.max = 100000
         option.visualMap.color = ['#FF6347','#ffad98']
+        barOption.series.itemStyle.color='#ee1212'
         option.tooltip.formatter= function(val) {
           if(val.data == null) return ;
           return '<span style="font-weight:bold">'+val.data.name +'</span>'+'<br/>'
@@ -168,160 +254,15 @@ export default {
             context.toolBarText2 + '<span style="color:red;font-weight:bolder">'+val.data.value[2]+'</span>'
         }
       }
-      chart.setOption(option)
-    },
-    /*getMap() {
-      const dom = document.getElementById('main');
-      // 初始化图表
-      //$(window).on('resize',function(){//
-        //屏幕大小自适应，重置容器高宽
-        //context.resizeMainContainer(dom);
-        //myChart.resize();
-      //});
-      this.resizeMainContainer(dom);
-      const myChart = echarts.init(dom, null, {
-        renderer: 'canvas',
-        useDirtyRect: false
-      });
-      const ROOT_PATH = 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
-      let option;
-      myChart.showLoading();
-      $.when(
-        $.get(ROOT_PATH + '/data/asset/geo/USA.json'),
-        $.getScript('https://fastly.jsdelivr.net/npm/d3-array'),
-        $.getScript('https://fastly.jsdelivr.net/npm/d3-geo')
-      ).done(function (res) {
-        const usaJson = res[0];
-        const projection = d3.geoAlbersUsa();
-        myChart.hideLoading();
-        echarts.registerMap('USA', usaJson);
-        option = {
-          title: {
-            text: 'USA Population Estimates (2012)',
-            subtext: 'Data from www.census.gov',
-            sublink: 'http://www.census.gov/popest/data/datasets.html',
-            left: 'right'
-          },
-          tooltip: {
-            trigger: 'item',
-            showDelay: 0,
-            transitionDuration: 0.2
-          },
-          visualMap: {
-            left: 'right',
-            min: 500000,
-            max: 38000000,
-            inRange: {
-              color: [
-                '#313695',
-                '#4575b4',
-                '#74add1',
-                '#abd9e9',
-                '#e0f3f8',
-                '#ffffbf',
-                '#fee090',
-                '#fdae61',
-                '#f46d43',
-                '#d73027',
-                '#a50026'
-              ]
-            },
-            text: ['High', 'Low'],
-            calculable: true
-          },
-          toolbox: {
-            show: true,
-            //orient: 'vertical',
-            left: 'left',
-            top: 'top',
-            feature: {
-              dataView: { readOnly: false },
-              restore: {},
-              saveAsImage: {}
-            }
-          },
-          series: [
-            {
-              name: 'USA PopEstimates',
-              type: 'map',
-              map: 'USA',
-              projection: {
-                project: function (point) {
-                  return projection(point);
-                },
-                unproject: function (point) {
-                  return projection.invert(point);
-                }
-              },
-              emphasis: {
-                label: {
-                  show: true
-                }
-              },
-              data: [
-                { name: 'Alabama', value: 4822023 },
-                { name: 'Alaska', value: 731449 },
-                { name: 'Arizona', value: 6553255 },
-                { name: 'Arkansas', value: 2949131 },
-                { name: 'California', value: 38041430 },
-                { name: 'Colorado', value: 5187582 },
-                { name: 'Connecticut', value: 3590347 },
-                { name: 'Delaware', value: 917092 },
-                { name: 'District of Columbia', value: 632323 },
-                { name: 'Florida', value: 19317568 },
-                { name: 'Georgia', value: 9919945 },
-                { name: 'Hawaii', value: 1392313 },
-                { name: 'Idaho', value: 1595728 },
-                { name: 'Illinois', value: 12875255 },
-                { name: 'Indiana', value: 6537334 },
-                { name: 'Iowa', value: 3074186 },
-                { name: 'Kansas', value: 2885905 },
-                { name: 'Kentucky', value: 4380415 },
-                { name: 'Louisiana', value: 4601893 },
-                { name: 'Maine', value: 1329192 },
-                { name: 'Maryland', value: 5884563 },
-                { name: 'Massachusetts', value: 6646144 },
-                { name: 'Michigan', value: 9883360 },
-                { name: 'Minnesota', value: 5379139 },
-                { name: 'Mississippi', value: 2984926 },
-                { name: 'Missouri', value: 6021988 },
-                { name: 'Montana', value: 1005141 },
-                { name: 'Nebraska', value: 1855525 },
-                { name: 'Nevada', value: 2758931 },
-                { name: 'New Hampshire', value: 1320718 },
-                { name: 'New Jersey', value: 8864590 },
-                { name: 'New Mexico', value: 2085538 },
-                { name: 'New York', value: 19570261 },
-                { name: 'North Carolina', value: 9752073 },
-                { name: 'North Dakota', value: 699628 },
-                { name: 'Ohio', value: 11544225 },
-                { name: 'Oklahoma', value: 3814820 },
-                { name: 'Oregon', value: 3899353 },
-                { name: 'Pennsylvania', value: 12763536 },
-                { name: 'Rhode Island', value: 1050292 },
-                { name: 'South Carolina', value: 4723723 },
-                { name: 'South Dakota', value: 833354 },
-                { name: 'Tennessee', value: 6456243 },
-                { name: 'Texas', value: 26059203 },
-                { name: 'Utah', value: 2855287 },
-                { name: 'Vermont', value: 626011 },
-                { name: 'Virginia', value: 8185867 },
-                { name: 'Washington', value: 6897012 },
-                { name: 'West Virginia', value: 1855413 },
-                { name: 'Wisconsin', value: 5726398 },
-                { name: 'Wyoming', value: 576412 },
-                { name: 'Puerto Rico', value: 3667084 }
-              ]
-            }
-          ]
-        };
-        myChart.setOption(option);
-      });
-
-      if (option && typeof option === 'object') {
-        myChart.setOption(option);
+      if(optionType==='bar')
+      {
+        chart.setOption(barOption, true);
       }
-    },*/
+      else
+      {
+        chart.setOption(option, true)
+      }
+    },
   },
   mounted() {
     this.runWorldMap()
